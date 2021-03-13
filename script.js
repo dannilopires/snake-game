b8c3ba57e8cbe876b267263ccd4b9a5f8e87459b
dannilo <dannilo-pires@hotmail.com>
2021-03-13 20:51:51 -0300

    add metodo controle

--- script.js
+++ script.js
@@ -56,5 +56,26 @@ function startGame(){
 
 let game = setInterval(startGame, 100);
 
+document.addEventListener('keydown', update);
+
+function update(event){
+
+	if(event.keyCode == 37 && direction != "right") {
+		direction = "left";
+	}
+
+	if (event.keyCode == 38 && direction != "down"){
+		direction = "up";
+	}
+
+	if (event.keyCode == 39 && direction != "left"){
+		direction = "right";
+	}
+
+	if (event.keyCode == 40 && direction != "up") {
+		direction = "down";
+	}
+
+}
 
 
