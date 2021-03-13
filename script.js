390a436aaf67b3febfd6f34ee5f55eee88b5d367
dannilo <dannilo-pires@hotmail.com>
2021-03-13 08:47:00 -0300

    add método criarsnake()

--- script.js
+++ script.js
@@ -1,10 +1,25 @@
 let canvas = document.getElementById("snake");
 let context = canvas.getContext('2d');
 let box = 32;
+let snake = [];
+snake[0] ={
+    x: 10*box,
+    y: 10* box
+}
 
 function criarBG() {
     context.fillStyle = "lightgreen";
     context.fillRect(0, 0, 16*box, 16*box);
 }
 
+function criarSnake(){
+    for(i=0; i < snake.length; i++) {
+        context.fillStyle = "black";
+        context.fillRect(snake[i].x, snake[i].y, box, box);
+    }
+
+}
+
 criarBG();
+
+criarSnake();
