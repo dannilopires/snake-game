833a0c25b4c4d8bb14e5ee8450f46383fbf1c060
dannilo <dannilo-pires@hotmail.com>
2021-03-13 18:17:30 -0300

    ajuste no width/height do canvas, add metodo startGame e metodo setInterval

--- script.js
+++ script.js
@@ -2,24 +2,59 @@ let canvas = document.getElementById("snake");
 let context = canvas.getContext('2d');
 let box = 32;
 let snake = [];
-snake[0] ={
-    x: 10*box,
-    y: 10* box
+snake[0] = {
+    x: 8 * box,
+    y: 8 * box
+};
+
+let direction = "right";
+
+function criarBg() {
+    context.fillStyle = "lightgreen";
+    context.fillRect(0, 0, 16 * box, 16 * box);
 }
 
-function criarBG() {
-    context.fillStyle = "lightgreen";
-    context.fillRect(0, 0, 16*box, 16*box);
-}
 
 function criarSnake(){
     for(i=0; i < snake.length; i++) {
-        context.fillStyle = "black";
+        context.fillStyle = "Black";
         context.fillRect(snake[i].x, snake[i].y, box, box);
     }
 
 }
 
-criarBG();
+function startGame(){
+	criarBg();
+	criarSnake();
+	
+	let snakeX = snake[0].x; 
+	let snakeY = snake[0].y;
 
-criarSnake();
+	
+	if(direction == "right"){
+		snakeX += box;
+	}
+	if(direction == "left") {
+		snakeX -= box;
+	}
+	if(direction == "up"){
+		snakeY -= box;
+	}  
+	if(direction == "down"){
+		snakeY += box;
+	}
+	
+	snake.pop();
+	
+	let newHead = {
+		x: snakeX,
+		y: snakeY
+	} 
+	
+	snake.unshift(newHead);
+}
+
+let game = setInterval(startGame, 100);
+
+
+
